406a6786a8aef0378c4eeeaf4f72784103fa06b7
Nikhil Tailor <85416176+NikhilTailor@users.noreply.github.com>
2021-09-07 09:34:37 +0530

    First commit

---
+++ Dimensions.py
@@ -0,0 +1,263 @@
+# Code for finding dimensions.
+
+while True:
+
+    print(
+"""
+Choose the number according to find the dimensions.
+1= length
+2= area
+3= volume
+4= velocity
+5= accelaration
+6= density
+7= linear momentum
+8= force
+9= pressure
+10= universal gravitational constant
+11= work , energy
+12= power
+13= surface tension
+14= force constant
+15= frequency
+16= angle
+17= solid angle
+18= angular velocity
+19= angular accelaration
+20= angular momentum
+21= stress
+22= strain
+23= torque
+24= plancks constant
+25= wavelength
+26= modulus of elasticity
+27= moment of inertia
+28= coefficient of viscosity
+29= moment of torque
+30= electric charge
+31= electric field intensity
+32= electric potential
+33= capacitance
+34= temperature
+35= entropy
+36= electric resistance
+37= inductance
+38= magnetic field
+39= magnetic flux
+40= magnetic moment
+41= magnetic pole strength
+42= stefan constant
+43= boltzmann constant
+44= conductance
+45= electric field
+46= electric flux
+47= electric dipole moment
+48= energy density
+49= potential energy.
+"""   )
+
+    op = input ('Enter the number from 1 to 49 according to above information :-')
+
+    if op=='1':
+        print('dimension= [M^0L^1T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='2':
+        print('dimension= [M^0L^2T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='3':
+        print('dimension= [M^0L^3T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='4':
+        print('dimension= [M^0L^1T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='5':
+        print('dimension= [M^L^1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='6':
+        print('dimension= [M^1L^-3T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='7':
+        print('dimension= [M^1L^1T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='8':
+        print('dimension= [M^1L^1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='9':
+        print('dimension= [M^1L^-1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='10':
+        print('dimension= [M^-1L^3T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='11':
+        print('dimension= [M^1L^2T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='12':
+        print('dimension= [M^1L^2T^-3]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='13':
+        print('dimension= [M^1L^0T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='14':
+        print('dimension= [M^1L^0T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='15':
+        print('dimension= [M^0L^0T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='16':
+        print('dimension= [M^0L^0T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='17':
+        print('dimension= [M^0L^0T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='18':
+        print('dimension= [M^0L^0T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='19':
+        print('dimension= [M^0L^0T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='20':
+        print('dimension= [M^1L^2T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='21':
+        print('dimension= [M^1L^-1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='22':
+        print('dimension= [M^0L^0T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='23':
+        print('dimension= [M^1L^2T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='24':
+        print('dimension= [M^1L^2T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='25':
+        print('dimension= [M^0L^1T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='26':
+        print('dimension= [M^1L^-1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='27':
+        print('dimension= [M^1L^2T^0]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='28':
+        print('dimension= [M^1L^-1T^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='29':
+        print('dimension= [M^1L^2T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='30':
+        print('dimension= [M^0L^0T^1A^1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='31':
+        print('dimension= [M^1L^1T^-3A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='32':
+        print('dimension= [M^1L^2T^-3A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='33':
+        print('dimension= [M^-1L^-2T^4A^1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='34':
+        print('dimension= [K]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='35':
+        print('dimension= [M^1L^2T^-2K^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='36':
+        print('dimension= [M^1L^2T^-3A^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='37':
+        print('dimension= [M^1L^2T^-2A^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='38':
+        print('dimension= [M^1L^0T^-2A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='39':
+        print('dimension= [M^1L^2T^-2A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='40':
+        print('dimension= [M^0L^2T^0A^1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='41':
+        print('dimension= [M^0L^1T^0A^1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='42':
+        print('dimension= [M^1L^0T^-3K^-4]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='43':
+        print('dimension= [M^1L^2T^-2K^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='44':
+        print('dimension= [M^-1L^-2T^3A^2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='45':
+        print('dimension= [M^1L^1T^-3A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='46':
+        print('dimension= [M^1L^3T^-3A^-1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='47':
+        print('dimension= [M^0L^1T^1A^1]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='48':
+        print('dimension= [M^1L^-1T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+    elif op=='49':
+        print('dimension= [M^1L^2T^-2]')
+        choice = input('Press enter to continue or e/E to exit... ')
+
+    else:
+        print('Access denied!')
+
+    if choice in ('e','E'):
+        break
+print('Code exit successfully...')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
--- Readme.txt
+++ Readme.txt
@@ -1,20 +1,8 @@
-Welcome the Periodic Table.
+Welcome to the Dimensions...
 
-In this software the program takes the input from the user of atomic nnumber from 1 to 118,
-and displays the information of the following data:-
-
-*Name of the element with its symbol
-*Atomic mass
-*Valency
-*Electronic configuration
-*Position in the periodic table.
-
-When you are done with the searching of a data , the program will ask for whether you want to
-find more or not, then press either 'y' or 'n'.
-If pressed 'y', the program will run further again and if pressed 'n', the program will exit
-and the console will end automatically...
-
-
+In this code you may find the 50 different dimensions which were prorammed in it.
+Just input the number given to the formula and press enter the dimension will be 
+displayed on your screen.
 
 Creator
 Nikhil Tailor.
